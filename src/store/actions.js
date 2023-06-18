82513150e61bb5a4c8dd831f53436ac2ec694fad
AvinashPayak <avinashpayak@gmail.com>
2023-06-18 05:39:13 +0530

    removed debugger

--- src/store/actions.js
+++ src/store/actions.js
@@ -3,6 +3,5 @@ import axiosClient from '../utils/axiosCLient';
 export async function searchMeal({ commit }, keyword){
     const url = `search.php?s=${keyword}`
     const { data } = await axiosClient.get(url); 
-    debugger;
     commit('setSearchedMeals', data.meals);
 }
