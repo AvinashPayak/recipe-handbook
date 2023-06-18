7ad730399b9ff0bfdf4c77ef72d8d7ac7406497b
AvinashPayak <avinashpayak@gmail.com>
2023-06-18 05:38:25 +0530

    resolved error

--- src/store/actions.js
+++ src/store/actions.js
@@ -3,5 +3,6 @@ import axiosClient from '../utils/axiosCLient';
 export async function searchMeal({ commit }, keyword){
     const url = `search.php?s=${keyword}`
     const { data } = await axiosClient.get(url); 
-\    commit('setSearchedMeals', data.meals);
+    debugger;
+    commit('setSearchedMeals', data.meals);
 }
